77d9c9b0adf121c5e1ee5f8738a469bdb6024fb3
Pallavi Gupta <Pallavi Gupta>
2016-06-19 21:15:56 +0530

    project work
    Signed-off-by: Pallavi Gupta <Pallavi Gupta>

---
+++ accessories/src/accessories/controller/Login.java
@@ -0,0 +1,50 @@
+package accessories.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.jni.User;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		String UserName=request.getParameter("t1");
+		String Password=request.getParameter("t2");
+		
+		user u =new user();
+		
+		
+		u.setUserName(UserName);
+		u.setPassword(Password);	}
+
+}
